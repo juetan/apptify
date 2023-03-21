c15b9fc222eae31f25d42adf4af6388bc93a0ddf
juetan <contact@juetan.cn>
2023-03-21 22:14:03 +0800

    feat: 优化包名

---
+++ packages/create-apptify/vite.config.ts
@@ -0,0 +1,18 @@
+import { builtinModules } from 'module';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/main.ts',
+      fileName: 'index',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: [...builtinModules],
+      output: {
+        banner: `#!/usr/bin/env node`,
+      },
+    },
+  },
+});
